29dd86adf66d54ea0fadbf76a6405be3dd1f2e22
Safwan Samsudeen <safwansamsudeen@gmail.com>
2021-07-01 13:56:32 +0530

    v2 with indexing

--- indexer.py
+++ indexer.py
@@ -1,29 +1,9 @@
 from pathlib import Path
 import json
+
+DIRS = ['/Applications', '/Users/safwan']
 EXCLUDED_DIRS = ['Library', 'opt', 'node_modules']
 EXCLUDED_EXTS = ['.plist']
-
-
-def indexer(dir='/Users/safwan', index=None):
-    if index is None:
-        index = {}
-    for path in filter(path_black_list_conditions, Path(dir).iterdir()):
-        try:
-            path.iterdir()
-            index = indexer(path, index)
-        except (PermissionError, NotADirectoryError):
-            pass
-        path_name = path.name
-        index.setdefault(path_name[0], {'paths': [], 'chs': {}})
-        sub_index = index
-        for i, ch in enumerate(path_name):
-            sub_index.setdefault(ch, {'paths': [], 'chs': {}})
-            if i == len(path_name) - 1:
-                sub_index[ch]['paths'].append(str(path.resolve()))
-                break
-            sub_index = sub_index[ch]['chs']
-    print(f'{dir} done.')
-    return index
 
 
 def path_black_list_conditions(file):
@@ -33,6 +13,32 @@ def path_black_list_conditions(file):
         any(file.name.endswith(ext) for ext in EXCLUDED_EXTS))
 
 
-index = indexer()
+def indexer(dirs, index=None):
+    if index is None:
+        index = []
+
+    for dir in dirs:
+        for path in filter(path_black_list_conditions, Path(dir).iterdir()):
+            if is_folder(path):
+                indexer([path], index)
+
+            index.append({
+                'path': str(path.resolve()),
+            })
+        print(f'{dir} done.')
+    return index
+
+
+def is_folder(path):
+    if path.suffix == '.app':
+        return False
+    try:
+        list(path.iterdir())
+    except (PermissionError, NotADirectoryError):
+        return False
+    return True
+
+
+index = indexer(DIRS)
 with open('index.json', 'w') as f:
     json.dump(index, f)
